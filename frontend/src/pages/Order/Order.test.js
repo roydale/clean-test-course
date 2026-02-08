bef8c0aaaf30c1849473e412a6ce70a8b37c1c9c
Roy Dale Caliwag <157586512+roydale@users.noreply.github.com>
2026-02-08 23:24:18 +0800

    Added and automated fucntional tests

--- frontend/src/pages/Order/Order.test.js
+++ frontend/src/pages/Order/Order.test.js
@@ -1,22 +1,22 @@
-import * as React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { API_URL } from '../../utils/constants';
-import axios from 'axios';
-import Order from '.';
-import OrderContext from '../../context/OrderContext';
+import * as React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { API_URL } from "../../utils/constants";
+import axios from "axios";
+import Order from ".";
+import OrderContext from "../../context/OrderContext";
 
-describe('Test Order', () => {
+describe("Test Order", () => {
   let orderName;
   let orderItems;
   beforeEach(() => {
     //Arrange:
     //Setup Order Context
-    orderName = 'test-fun';
+    orderName = "test-fun";
     orderItems = [
-      { item: 'Test 1', quantity: 1 },
-      { item: 'Test 2', quantity: 2 },
-      { item: 'Test 3', quantity: 3 },
+      { item: "Test 1", quantity: 1 },
+      { item: "Test 2", quantity: 2 },
+      { item: "Test 3", quantity: 3 },
     ];
   });
 
@@ -24,7 +24,7 @@ describe('Test Order', () => {
     jest.clearAllMocks();
   });
 
-  test('Test Delivery Fee', async () => {
+  test("Test Delivery Fee", async () => {
     //Add a Test to verify that delivery fee shows up here
     //Act:
     //Setup the Mock API
@@ -33,15 +33,15 @@ describe('Test Order', () => {
     render(
       <OrderContext.Provider value={{ orderName, orderItems }}>
         <Order />
-      </OrderContext.Provider>
+      </OrderContext.Provider>,
     );
     //Assert: replace the return true.
     await waitFor(() => {
-      return true;
+      expect(screen.getAllByText("$2.50")).toHaveLength(1);
     });
   });
 
-  test('Test Update Delivery Fee', async () => {
+  test("Test Update Delivery Fee", async () => {
     //Modify the delivery distance and verify that the delivery fee is updated
     //Act:
     //Setup the Mock API
@@ -50,47 +50,47 @@ describe('Test Order', () => {
     render(
       <OrderContext.Provider value={{ orderName, orderItems }}>
         <Order />
-      </OrderContext.Provider>
+      </OrderContext.Provider>,
     );
 
     //ACT
     //Update the Delivery distance by choosing the 5 mile option from the drop down
     userEvent.selectOptions(
       // Find the select element, like a real user would.
-      screen.getByRole('combobox'),
+      screen.getByRole("combobox"),
       // Find and select the 5 mile option, like a real user would.
-      screen.getByRole('option', { name: '5 miles' })
+      screen.getByRole("option", { name: "5 miles" }),
     );
     //Assert: replace the return true.
     await waitFor(() => {
-      return true;
+      expect(screen.getAllByText("$5.00")).toHaveLength(1);
     });
   });
 });
 
 const setupMock = () => {
   //Mock API calls
-  const mockGet = jest.spyOn(axios, 'get');
+  const mockGet = jest.spyOn(axios, "get");
   mockGet.mockImplementation((url) => {
     switch (url) {
       case `${API_URL}/api/delivery/test-fun/0`:
         return Promise.resolve({
           data: {
-            status: 'success',
+            status: "success",
             data: 2.5,
           },
         });
       case `${API_URL}/api/delivery/test-fun/5`:
         return Promise.resolve({
           data: {
-            status: 'success',
+            status: "success",
             data: 5.0,
           },
         });
       default:
         return Promise.resolve({
           data: {
-            status: 'fail',
+            status: "fail",
           },
         });
     }
