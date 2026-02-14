501d73befa63419de47755f6fb56f5297120d92d
Roy Dale Caliwag <157586512+roydale@users.noreply.github.com>
2026-02-14 21:17:46 +0800

    Added setup for Cypress acceptance test

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -1,11 +1,19 @@
-describe('Shows Appeteasers', () => {
-  it('Connect to Dev Server', () => {
-    cy.visit('https://hangryhippo.quantic.host/');
+describe("Shows Appeteasers", () => {
+  it("Connect to Dev Server", () => {
+    cy.visit("https://hangryhippo.quantic.host/");
   });
-  it('selects Handhelds', () => {
+  it("selects Handhelds", () => {
     //TODO: Add a test to check Handhelds
+    cy.contains("Handhelds").click();
+    cy.contains("Cheese Burger").should("be.visible");
+    cy.contains("Fajita Tacos").should("be.visible");
   });
-  it('selects Appeteasers', () => {
+  it("selects Appeteasers", () => {
     //TODO: Add a test to check Appeteasers
+    cy.contains("Appeteasers").click();
+    cy.contains("Tater Tots").should("be.visible");
+    cy.contains("Buffalo Wings").should("be.visible");
+    cy.contains("Cheese Burger").should("not.exist");
+    cy.contains("Fajita Tacos").should("not.exist");
   });
 });
